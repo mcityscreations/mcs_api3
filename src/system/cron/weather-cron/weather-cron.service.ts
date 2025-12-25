2aeb740d1942a3638f55d5f2f5d18658c90c94b9
Yves-Leonardo Marchon <yves.marchon@gmail.com>
2025-12-26 00:33:04 +0100

    Replacing console.log by WLS

--- src/system/cron/weather-cron/weather-cron.service.ts
+++ src/system/cron/weather-cron/weather-cron.service.ts
@@ -4,10 +4,14 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { WeatherService } from '../../../weather/weather.service';
 import { isErrorWithMessage } from '../../../common/types/error.types';
+import { WinstonLoggerService } from '../../logger/logger-service/winston-logger.service';
 
 @Injectable()
 export class WeatherCronService {
-	constructor(private readonly weatherService: WeatherService) {}
+	constructor(
+		private readonly weatherService: WeatherService,
+		private readonly logger: WinstonLoggerService,
+	) {}
 
 	// Configuring Cron task
 	@Cron('0 * * * *', {
@@ -15,17 +19,24 @@ export class WeatherCronService {
 		timeZone: 'Europe/Paris',
 	})
 	async handleWeatherUpdate() {
-		console.log(`--- Executing Cron task - Updating weather data...) ---`);
+		this.logger.log('--- Executing Cron task - Updating weather data... ---', {
+			context: 'WeatherCronService',
+		});
 
 		try {
 			// Calling weather service
 			await this.weatherService.setWeather();
-			console.log('Weather successfully uploaded.');
+			this.logger.log('Weather data updated successfully.', {
+				context: 'WeatherCronService',
+			});
 		} catch (error) {
 			if (isErrorWithMessage(error)) {
-				console.error(
-					'Unable to retrieve current weather in Marseille :',
-					error.message,
+				this.logger.error(
+					'Failed to retrieve current weather in Marseille.',
+					error.stack,
+					{
+						context: 'WeatherCronService',
+					},
 				);
 			}
 		}
