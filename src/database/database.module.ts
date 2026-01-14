5cc7322b5f055ecb64601d9a3323e1e88233ee62
Yves-Leonardo Marchon <yves.marchon@gmail.com>
2026-01-14 11:39:01 +0100

    TypeORM is being removed because it is not suitable for the complexity of this project.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,67 +5,44 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 // Database engines
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgreSQLService } from './postgresql/postgresql.service';
 import { RedisService } from './redis/redis.service';
-import { MariaDBService } from './maria-db/maria-db.service';
 
 // Configuration services and interfaces
-import { MariaDbConfigService } from './maria-db/maria-db-config/maria-db-config.service';
-import type { ISQLDatabaseConfig } from './database.interfaces';
 import {
 	IRedisConfig,
 	RedisConfigService,
 } from './redis/redis-config/redis-config.service';
-import { TypeormConfigService } from './typeorm/typeorm-config/typeorm-config.service';
-import { SystemModule } from '../system/system.module';
+import { PostgresqlConfigService } from './postgresql/postgresql-config/postgresql-config.service';
 
 // Logging
-import { Logger } from 'winston';
-import { WINSTON_LOGGER } from '../system/logger/logger-factory/winston-logger.factory';
+import { WinstonLoggerService } from 'src/system/logger/logger-service/winston-logger.service';
 
 @Module({
-	imports: [
-		SystemModule,
-		ConfigModule,
-		// TypeORM module with asynchronous configuration
-		TypeOrmModule.forRootAsync({
-			inject: [TypeormConfigService],
-			useFactory: (service: TypeormConfigService) =>
-				service.getPostgresCoreConfig(),
-		}),
-	],
+	// No need to import SystemModule here as it's global
+	imports: [ConfigModule],
 	providers: [
 		RedisService,
-		MariaDBService,
-		MariaDbConfigService,
-		// Provider that generates the STANDARD DB config object
+		PostgresqlConfigService,
+		// Config objects for PostgreSQL connections
 		{
-			provide: 'STANDARD_DB_CONFIG',
-			useFactory: (configService: MariaDbConfigService) =>
-				configService.getStandardConfig(),
-			inject: [MariaDbConfigService],
+			provide: 'PG_STANDARD_CONFIG',
+			useFactory: (config: PostgresqlConfigService) =>
+				config.getStandardConfig(),
+			inject: [PostgresqlConfigService],
 		},
-		// Provider that generates the OAUTH DB config object
 		{
-			provide: 'OAUTH_DB_CONFIG',
-			useFactory: (configService: MariaDbConfigService) =>
-				configService.getSecurityConfig(),
-			inject: [MariaDbConfigService],
+			provide: 'PG_OAUTH_CONFIG',
+			useFactory: (config: PostgresqlConfigService) =>
+				config.getSecurityConfig(),
+			inject: [PostgresqlConfigService],
 		},
-
-		// MariaDBService (relies on the two config providers above)
+		// Postgres SQL engine service
 		{
-			provide: MariaDBService,
-			useFactory: (
-				standardConfig: ISQLDatabaseConfig,
-				oauthConfig: ISQLDatabaseConfig,
-				logger: Logger,
-			) => {
-				// Factory function that creates the MariaDBService instance
-				return new MariaDBService(standardConfig, oauthConfig, logger);
-			},
-			// Injecting the two config objects
-			inject: ['STANDARD_DB_CONFIG', 'OAUTH_DB_CONFIG', WINSTON_LOGGER],
+			provide: PostgreSQLService,
+			useFactory: (stdCfg: any, authCfg: any, logger: WinstonLoggerService) =>
+				new PostgreSQLService(stdCfg, authCfg, logger),
+			inject: ['PG_STANDARD_CONFIG', 'PG_OAUTH_CONFIG', WinstonLoggerService],
 		},
 		// Provider that generates the REDIS_CONFIG config object
 		{
@@ -77,16 +54,15 @@ import { WINSTON_LOGGER } from '../system/logger/logger-factory/winston-logger.f
 		// RedisConfigService (relies on the config provider above)
 		{
 			provide: RedisService,
-			useFactory: (redisConfig: IRedisConfig, logger: Logger) => {
+			useFactory: (redisConfig: IRedisConfig, logger: WinstonLoggerService) => {
 				// Factory function that creates the RedisService instance
 				return new RedisService(redisConfig, logger);
 			},
 			// Injecting the config object
-			inject: ['REDIS_CONFIG', WINSTON_LOGGER],
+			inject: ['REDIS_CONFIG', WinstonLoggerService],
 		},
-		TypeormConfigService,
 	],
 	// Exporting services for use in other modules
-	exports: [RedisService, MariaDBService, MariaDbConfigService, TypeOrmModule],
+	exports: [RedisService, PostgreSQLService, PostgresqlConfigService],
 })
 export class DatabaseModule {}
