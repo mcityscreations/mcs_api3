12e38c196414f2648475b3a6c043e3ce5c9cf999
Yves-Léonardo Marchon <mcityscreations@gmail.com>
2026-01-30 00:05:36 +0100

    Creating Keyword DTOs

---
+++ src/taxonomy/keywords/dtos/read-keyword.dto.ts
@@ -0,0 +1,5 @@
+// src/taxonomy/keywords/dtos/read-keyword.dto.ts
+import { createZodDto } from 'nestjs-zod';
+import { ReadKeywordSchema } from '../schemas/keywords.schemas.js';
+
+export class ReadKeywordDto extends createZodDto(ReadKeywordSchema) {}
--- src/taxonomy/keywords/schemas/keywords.schemas.ts
+++ src/taxonomy/keywords/schemas/keywords.schemas.ts
@@ -1,1 +1,45 @@
-import z from "zod";
+// src/taxonomy/keywords/schemas/keywords.schemas.ts
+import z from 'zod';
+
+// Schema for creating a new keyword
+export const CreateKeywordSchema = z.object({
+	name: z
+		.array(
+			z.object({
+				idLanguage: z.uuidv7(),
+				value: z.string().min(1).max(100),
+			}),
+		)
+		.min(1),
+});
+export type ICreateKeywordDto = z.infer<typeof CreateKeywordSchema>;
+
+// Schema for reading an existing keyword
+export const ReadKeywordSchema = z.object({
+	id: z.uuidv7().describe('Unique identifier for the keyword'),
+	name: z.string().min(1).max(100).describe('Value of the keyword'),
+	createdAt: z.coerce
+		.date()
+		.optional()
+		.describe('Timestamp when the keyword was created'),
+	updatedAt: z.coerce
+		.date()
+		.optional()
+		.describe('Timestamp when the keyword was last updated'),
+});
+export type IReadKeywordDto = z.infer<typeof ReadKeywordSchema>;
+
+// Schema for updating an existing keyword
+export const UpdateKeywordSchema = z.object({
+	id: z.uuidv7(),
+	name: z
+		.array(
+			z.object({
+				idLanguage: z.uuidv7(),
+				value: z.string().min(1).max(100),
+			}),
+		)
+		.min(1)
+		.optional(),
+});
+export type IUpdateKeywordDto = z.infer<typeof UpdateKeywordSchema>;
