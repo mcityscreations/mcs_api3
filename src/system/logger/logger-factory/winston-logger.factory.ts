4637e1d538aca7888e151521cc37d9fd001e475f
Yves-Léonardo Marchon <mcityscreations@gmail.com>
2026-02-06 12:04:23 +0100

    Logger factory : Removing duplicate content. AlsService was used both in the factory and in the Logger service.

--- src/system/logger/logger-factory/winston-logger.factory.ts
+++ src/system/logger/logger-factory/winston-logger.factory.ts
@@ -1,10 +1,9 @@
-// src/common/logger/winston-logger.factory.ts
+// src/system/logger/logger-factory/winston-logger.factory.ts
 
 import { transports, format, createLogger, Logger } from 'winston';
 import { MongoDB, MongoDBConnectionOptions } from 'winston-mongodb';
 import { LoggerConfigService } from '../logger-config/logger-config.service.js';
 import { InternalServerErrorException } from '@nestjs/common';
-import { AlsService } from '../../als/als.service.js';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
 // MongoDB transport interface
@@ -21,69 +20,60 @@ export const winstonLoggerFactory = {
 	provide: WINSTON_LOGGER,
 	useFactory: (
 		configService: LoggerConfigService,
-		alsService: AlsService,
 	): Logger => {
+
+		// Loading MongoDB URI from configuration
 		const mongoUri = configService.getMongoUri();
 		if (!mongoUri)
-			throw new InternalServerErrorException(
-				'MongoDB configuration is missing',
-			);
-		const mongoTransport = new MongoDB({
-			// Connection configs
-			db: mongoUri,
-			collection: 'application_logs',
-			options: {
-				useUnifiedTopology: true,
-			},
-			level: 'warn',
-			capped: true, // Creates a limited size collection
-			cappedSize: 20000000, // 20 MB
-			format: format.combine(format.timestamp(), format.json()),
-		} as WinstonMongoDBOptions);
-
-		const correlationIdFormat = format((info) => {
-			const correlationId = alsService.getCorrelationId(); // Retrieving correlationId from ALS
-			if (correlationId) {
-				info.correlationId = correlationId;
-			}
-			return info;
-		})();
+		throw new InternalServerErrorException(
+			'MongoDB configuration is missing',
+		);
+		const transportsList = [
+			// 1. Console transport for "debug" messages, on development mode
+			new transports.Console({
+				level: 'debug',
+				format: format.combine(
+					format.colorize(),
+					format.printf(({ level, message, timestamp, requestId }) => {
+						// Converting type from unknown to string
+						const msg = message as string;
+						const time = timestamp as string;
+						const reqId = requestId ? ` [${requestId}]` : '';
+						// Fallback in case timestamp is undefined
+						const finalTimestamp = time || new Date().toISOString();
+						return `[Nest] ${finalTimestamp} ${reqId} ${level}: ${msg}`;
+					}),
+				),
+			}),
+			new DailyRotateFile({
+				filename: './logs/app-%DATE%.log',
+				zippedArchive: true,
+				maxSize: '10m',
+				maxFiles: '5d',
+				level: 'info',
+			}),
+			// 2. MongoDB transport for production mode.
+			new MongoDB({
+				db: mongoUri,
+				collection: 'application_logs',
+				options: {
+					useUnifiedTopology: true,
+				},
+				level: 'warn',
+				capped: true, // Creates a limited size collection
+				cappedSize: 20000000, // 20 MB
+				format: format.combine(format.timestamp(), format.json()),
+			} as WinstonMongoDBOptions)
+		];
 
 		return createLogger({
 			// Global format for all transports
 			format: format.combine(
-				correlationIdFormat,
 				format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
 			),
-			transports: [
-				// 1. Console transport for "debug" messages, on development mode
-				new transports.Console({
-					level: 'debug',
-					format: format.combine(
-						format.colorize(),
-						format.printf(({ level, message, timestamp }) => {
-							// Converting type from unknown to string
-							const msg = message as string;
-							const time = timestamp as string;
-							// Fallback in case timestamp is undefined
-							const finalTimestamp = time || new Date().toISOString();
-							return `[Nest] ${finalTimestamp} ${level}: ${msg}`;
-						}),
-					),
-				}),
-				new DailyRotateFile({
-					filename: 'logs/app-%DATE%.log',
-					zippedArchive: true,
-					maxSize: '10m',
-					maxFiles: '5d',
-					level: 'info',
-				}),
-				// 2. MongoDB transport for production mode.
-				// Logs 'info', 'warn' and 'error' messages
-				mongoTransport,
-			],
+			transports: transportsList,
 		});
 	},
 	// The Factory's dependency
-	inject: [LoggerConfigService, AlsService],
+	inject: [LoggerConfigService],
 };
