9a88d6b6a6b129c17b47b604005579bd6c547221
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-04-19 22:55:39 +0200

    Creating main artworks ' controller endpoints

--- src/modules/content/artworks/controller/artworks.controller.ts
+++ src/modules/content/artworks/controller/artworks.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import type { ICreateArtwork } from '../schemas/create-artwork.schema.js';
+import { ArtworksService } from '../services/artworks/artworks.service.js';
 
 @Controller('artworks')
-export class ArtworksController {}
+export class ArtworksController {
+    
+    constructor(private readonly artworksService: ArtworksService) {}
+
+    @Post()
+    addArtwork(artworkPayload: ICreateArtwork) {
+
+    }
+
+    @Get(':artworkId')
+    getArtwork(artworkId: string) {
+
+    }
+
+    getArtworksByCategory(categoryId: string) {
+
+    }
+
+    getArtworksByTechnique(techniqueId: string) {
+
+    }
+
+    getArtworksBySubject(subjectId: string) {
+
+    }
+
+    getArtworksByKeyword(keywordId: string) {
+
+    }
+}
--- src/modules/content/artworks/services/artworks/artworks.service.ts
+++ src/modules/content/artworks/services/artworks/artworks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ICreateArtwork } from '../../schemas/create-artwork.schema.js';
 
 @Injectable()
-export class ArtworksService {}
+export class ArtworksService {
+
+    addArtwork(artworkPayload: ICreateArtwork) {
+
+    }
+
+    getArtwork(artworkId: string) {
+
+    }
+
+    getArtworksByCategory(categoryId: string) {
+
+    }
+
+    getArtworksByTechnique(techniqueId: string) {
+
+    }
+
+    getArtworksBySubject(subjectId: string) {
+
+    }
+
+    getArtworksByKeyword(keywordId: string) {
+
+    }
+}
