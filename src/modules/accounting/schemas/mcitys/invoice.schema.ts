f474fc83ce5f56999cfc6f61969612900f8b1ff0
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-05-19 11:40:49 +0200

    New Mcitys invoice schema

---
+++ src/modules/accounting/schemas/mcitys/invoice.schema.ts
@@ -0,0 +1,82 @@
+/** Canonical Data Model : Common invoice schema to be used as an interface between Qonto,
+ * Prestashop and other accounting systems. All invoices transmitted by a third party API
+ * must be mapped to the following data structure before processing. */
+import { z } from 'zod';
+
+const currenciesEnum = z.enum([
+	'EUR',
+	'AED',
+	'AUD',
+	'BGN',
+	'CAD',
+	'CHF',
+	'CNY',
+	'CZK',
+	'DKK',
+	'GBP',
+	'GHS',
+	'HKD',
+	'HUF',
+	'ILS',
+	'MXN',
+	'NOK',
+	'NZD',
+	'PEN',
+	'PLN',
+	'RON',
+	'RSD',
+	'SAR',
+	'SEK',
+	'SGD',
+	'TRY',
+	'USD',
+	'ZAR',
+	'ZMW',
+]);
+
+export const McitysInvoiceSchema = z.object({
+	id_technical: z.string(), // Technical ID of the invoice in the third party system (Qonto, Prestashop, etc.)
+	reference: z.string(), // Reference of the invoice (e.g. invoice number)
+	issue_date: z.number().int().positive(), // Issue date of the invoice as a timestamp (UNIX timestamp)
+	due_date: z.number().int().positive(), // Due date of the invoice as a timestamp (UNIX timestamp)
+	currency: currenciesEnum, // Currency of the invoice (e.g. EUR, USD, etc.)
+	document_type: z.enum(['INVOICE', 'CREDIT_NOTE']).default('INVOICE'),
+	emitter: z
+		.object({
+			id: z.string(),
+			legal_number: z.string(), // Ton SIRET
+		})
+		.optional(),
+	customer: z.object({
+		id: z.string(), // Technical ID of the customer in the third party system (Qonto, Prestashop, etc.)
+		email: z.email().optional(), // Email of the customer
+		legal_number: z.string().optional(), // Legal number of the customer (e.g. SIRET in France)
+		country_code: z.string().length(2).default('FR'),
+	}),
+	items: z.array(
+		z.object({
+			id: z.string().min(1), // Technical ID of the item in the third party system (Qonto, Prestashop, etc.)
+			label: z.string().min(1), // Label of the item (e.g. product name)
+			description: z.string().nullable().optional(), // Description of the item
+			quantity: z.number().int().positive(), // Quantity of the item
+			unit_price_tax_excl: z.number().int().nonnegative(), // Unit price in cents of the item excluding tax
+			discount: z
+				.object({
+					type: z.enum(['percentage', 'absolute']), // Type of the discount (e.g. percentage, absolute)
+					value: z.number().int().positive(), // Value of the discount in Basis Points (e.g. 1000 for 10%) or in cents if 'absolute'
+				})
+				.optional(),
+			unit_price_tax_excl_discount: z.number().int().nonnegative().optional(), // Reduced unit price in cents of the item excluding tax
+			unit_price_tax_incl: z.number().int().nonnegative(), // Unit price in cents of the item including tax
+			vat_rate: z.number().int().nonnegative(), // Basis Point of the VAT rate of the item (e.g. 2000 for 20%)
+			total_price_tax_excl: z.number().int().nonnegative(), // Total price in cents of the item excluding tax
+			total_price_tax_incl: z.number().int().nonnegative(), // Total price in cents of the item including tax
+		}),
+	),
+	total_amount_tax_excl: z.number().int().nonnegative(), // Total amount in cents of the invoice excluding tax
+	total_amount_tax_incl: z.number().int().nonnegative(), // Total amount in cents of the invoice including tax
+	vat_amount: z.number().int().nonnegative(), // Total amount in cents of the VAT of the invoice
+	discount_amount: z.number().int().nonnegative(), // Total amount in cents of the discount of the invoice
+	payment_status: z.enum(['paid', 'unpaid', 'draft']), // Payment status of the invoice
+});
+export type IMcitysInvoice = z.infer<typeof McitysInvoiceSchema>;
