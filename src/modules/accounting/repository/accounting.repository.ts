472c9af3681a8c99a09db419392823ce2ff81b45
Mcitys créations <mcityscreations@gmail.com>
2026-06-22 11:54:12 +0200

    AccountingModule: Creating the squeleton of the saveInvoicesToDarabase method + adding the squeleton of the saveMainInvoiceData method into the Accounting Repository. To be developped later, real database fields unknown.

--- src/modules/accounting/repository/accounting.repository.ts
+++ src/modules/accounting/repository/accounting.repository.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PostgreSQLService } from '../../../system/database/postgresql/postgresql.service.js';
+import type { PoolClient } from 'pg';
+import type { IMcitysInvoice } from '../schemas/mcitys/invoice.schema.js';
 
 @Injectable()
-export class AccountingRepository {}
+export class AccountingRepository {
+    constructor(
+        private readonly dbService: PostgreSQLService,
+    ){}
+
+    public async saveMainInvoiceData(data: IMcitysInvoice, transactionClient: PoolClient): Promise<number | null> {
+        const sqlRequest = `INSERT INTO accounting.invoice (source_system) VALUES ($1) RETURNING id_invoice`;
+        const result = await this.dbService.execute<{id_invoice: number}>(sqlRequest, [data.source_system], 'standard', false, transactionClient);
+         return result.length > 0 ? result[0].id_invoice : null;
+    }
+}
--- src/modules/accounting/services/accounting.service.ts
+++ src/modules/accounting/services/accounting.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
+import { getErrorMessage } from '../../../common/utils/error.utils.js';
+import { WinstonLoggerService } from '../../../system/logger/logger-service/winston-logger.service.js';
+import { PostgreSQLService } from '../../../system/database/postgresql/postgresql.service.js';
+import type { PoolClient } from 'pg';
+import type { IMcitysInvoice } from '../schemas/mcitys/invoice.schema.js';
+import { AccountingRepository } from '../repository/accounting.repository.js';
 
 @Injectable()
 export class AccountingService {
+	constructor(
+		private readonly logger: WinstonLoggerService,
+		private readonly dbService: PostgreSQLService,
+		private readonly accountingRepository: AccountingRepository,
+	){}
 	public generateEReportingForPeriod(startDate: Date, endDate: Date) {
 		// The logic to generate the e-reporting file based
 		// on the invoices data stored in the database for a specific period
@@ -13,5 +25,36 @@ export class AccountingService {
 
 	public sendEinvoices() {
 		// The logic to send the generated e-invoices to the tax authorities
+	}
+
+	public async saveInvoicesToDatabase(
+		invoices: IMcitysInvoice[]
+	): Promise<void> {
+		this.logger.log(`Saving ${invoices.length} invoices to the database.`);
+
+		const transactionClient = await this.dbService.beginTransaction();
+
+		try {
+			for (const invoice of invoices) {
+				const idInvoice = await this.accountingRepository.saveMainInvoiceData(
+					invoice,
+					transactionClient
+				);
+
+				if (idInvoice === null) {
+					throw new InternalServerErrorException(
+						'Problem while saving main invoice data.'
+					);
+				}
+			}
+
+			await this.dbService.commit(transactionClient);
+		} catch (error) {
+			await this.dbService.rollback(transactionClient);
+			this.logger.error(getErrorMessage(error));
+			throw new InternalServerErrorException(getErrorMessage(error));
+		} finally {
+			transactionClient.release();
+		}
 	}
 }
