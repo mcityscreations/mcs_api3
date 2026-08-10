4683ee71574aad953586eb83c6c12fee4905e009
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-08-10 21:03:54 +0200

    CommonModule: Adding comments to the function getIDType() for a better dev experience.

--- src/common/utils/getIDType.utils.ts
+++ src/common/utils/getIDType.utils.ts
@@ -2,6 +2,10 @@ import { isUuidV7 } from '../validators/isuuidv7.validator.js';
 
 export type IiDType = 'public' | 'private' | 'invalid';
 
+/**
+ * @param incomingID It can be a private or public ID
+ * @returns 'public' | 'private' | 'invalid'
+ */
 export function getIDType(incomingID): IiDType {
 	if (isUuidV7(incomingID)) return 'private';
 	if (Number.isInteger(incomingID) && incomingID > 0) return 'public';
