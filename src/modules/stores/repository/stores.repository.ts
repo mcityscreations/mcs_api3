d544b444e1fa852ccc0e40b995e4ec110b34a36d
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-02 17:09:19 +0200

    Stores - Repository : Removing saveinvoice() method.

--- src/modules/stores/repository/stores.repository.ts
+++ src/modules/stores/repository/stores.repository.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PostgreSQLService } from '../../../system/database/postgresql/postgresql.service.js';
-import { ICreateMcitysInvoice } from '../../accounting/schemas/mcitys/invoice.schema.js';
 
 @Injectable()
 export class StoresRepository {
 	constructor(private readonly postgreSQLService: PostgreSQLService) {}
-
-	saveInvoice(invoiceData: ICreateMcitysInvoice): void {
-		// The logic to save the invoice data into the database
-	}
 
 	/** Retrieves the ID of the last Prestashop invoice recorded in the Mcitys database */
 	async getLastPrestashopInvoiceID(): Promise<number> {
