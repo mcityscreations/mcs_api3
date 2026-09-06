aaa5d94e890d7575434fb557b6833fdbf75c44e0
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-06 12:43:12 +0200

    test(accounting/repository): Adding mocked dependencies.

--- src/modules/accounting/repository/accounting.repository.spec.ts
+++ src/modules/accounting/repository/accounting.repository.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountingRepository } from './accounting.repository.js';
+import { PostgreSQLService } from '../../../system/database/postgresql/postgresql.service.js';
 
 describe('AccountingRepository', () => {
 	let service: AccountingRepository;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AccountingRepository],
+			providers: [
+				AccountingRepository,
+				{
+					provide: PostgreSQLService,
+					useValue: {
+						execute: jest.fn(),
+						beginTransaction: jest.fn(),
+						commit: jest.fn(),
+						rollback: jest.fn(),
+					},
+				},
+			],
 		}).compile();
 
 		service = module.get<AccountingRepository>(AccountingRepository);
