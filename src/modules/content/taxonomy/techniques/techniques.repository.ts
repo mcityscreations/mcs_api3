727350160e82ec2288056217bd8ca4d3350231be
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-16 11:54:56 +0200

    feat(taxonomy/techniques): The techniques service is now able to retrieve a technique from the database in the IAdminReadTechnique format.

--- src/modules/content/taxonomy/techniques/techniques.repository.ts
+++ src/modules/content/taxonomy/techniques/techniques.repository.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PostgreSQLService } from '../../../../system/database/postgresql/postgresql.service.js';
-import { Ii18n } from '../../../../common/schemas/i18n.schema.js';
+import { IAdminReadTechnique } from './schemas/technique.schemas.js';
 
 @Injectable()
 export class TechniquesRepository {
 	constructor(private readonly dbService: PostgreSQLService) {}
 
-	public async findOne(id: string): Promise<Ii18n[] | null> {
+	public async findOne(id: string): Promise<IAdminReadTechnique[] | null> {
 		const query = `SELECT
-                tcl.id_public as "idLanguage",
-                tti18n.title AS value 
-                tti18n.slug AS slug
-                FROM taxonomy.technique_i18n tti18n 
-                INNER JOIN taxonomy.language tcl ON tti18n.id_language = tcl.id_language
-                INNER JOIN taxonomy.technique tt ON tt.id_technique = tti18n.id_technique
-                WHERE tt.id_public = $1;`;
+		tt.id_public AS id,
+		tt.name AS name,
+		json_agg(json_build_object(
+			'idLanguage', tcl.id_public,
+			'title', tti18n.title,
+			'slug', tti18n.slug
+		) ORDER BY tcl.id_public) AS i18n,
+		tt.is_public AS "isPublic",
+		tt.created_at AS "createdAt",
+		tt.updated_at AS "updatedAt"
+		FROM taxonomy.technique_i18n tti18n 
+		INNER JOIN taxonomy.language tcl ON tti18n.id_language = tcl.id_language
+		INNER JOIN taxonomy.technique tt ON tt.id_technique = tti18n.id_technique
+		WHERE tt.id_public = $1
+		GROUP BY 
+			tt.id_public,
+			tt.name,
+			tt.is_public,
+			tt.created_at,
+			tt.updated_at;`;
 
-		const result: Ii18n[] = await this.dbService.execute(
+		const result: IAdminReadTechnique[] = await this.dbService.execute(
 			query,
 			[id],
 			'standard',
