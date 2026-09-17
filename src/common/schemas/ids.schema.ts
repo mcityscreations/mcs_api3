745b3464b518e05e180c4a4e4b242c78a06f16a5
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-17 19:12:20 +0200

    feat(common/dtos): New uuidv7 DTO.

--- src/common/schemas/ids.schema.ts
+++ src/common/schemas/ids.schema.ts
@@ -6,3 +6,8 @@ export const IdsSchema = z.object({
 });
 
 export type IIds = z.infer<typeof IdsSchema>;
+
+export const UuidSchema = z.object({
+	id: z.uuidv7(),
+});
+export type IUuid = z.infer<typeof UuidSchema>;
