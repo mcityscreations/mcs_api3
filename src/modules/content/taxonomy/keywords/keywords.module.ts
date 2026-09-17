ededcc47752235c437e955a390816afb79c63fb2
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-17 18:05:42 +0200

    fix(taxonomy/keywords): Removing SystemModule from the import's list as it is a global module.

--- src/modules/content/taxonomy/keywords/keywords.module.ts
+++ src/modules/content/taxonomy/keywords/keywords.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { KeywordsController } from './controller/keywords.controller.js';
 import { KeywordsService } from './service/keywords.service.js';
 import { KeywordsRepository } from './repository/keywords.repository.js';
-import { SystemModule } from '../../../../system/system.module.js';
 import { SecurityModule } from '../../../security/security.module.js';
 
 @Module({
-	imports: [SystemModule, SecurityModule],
+	imports: [SecurityModule],
 	controllers: [KeywordsController],
 	providers: [KeywordsService, KeywordsRepository],
 	exports: [KeywordsService],
