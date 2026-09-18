954273727813fbe3cd83beff591a90204d1e67bf
Yves-Leonardo Marchon <mcityscreations@gmail.com>
2026-09-19 00:48:52 +0200

    fix(artwork/schema): The CreateArtwork schema is enriched with error messages and a better handling of dates. The date sent by the client is a string, Zod converts it into a Date object.

--- src/modules/content/artworks/schemas/create-artwork.schema.ts
+++ src/modules/content/artworks/schemas/create-artwork.schema.ts
@@ -1,16 +1,22 @@
 // src/modules/content/artworks/types/create-artwork.dto.ts
 
-import z from 'zod';
+import { z } from 'zod';
 import { CreateTitleSchema } from '../../../../common/schemas/title.schema.js';
 import { CreateDescriptionSchema } from '../../../../common/schemas/description.schema.js';
 import { DimensionsSchema } from '../../../../common/schemas/dimensions.schema.js';
 
 const CreateArtworkSchema = z.object({
-	idArtist: z.uuidv7(),
-	idCategory: z.uuidv7(),
-	idTechnique: z.uuidv7(),
-	idSubject: z.uuidv7(),
-	releaseDate: z.string(),
+	idArtist: z.uuidv7('An artist ID must be provided'),
+	idCategory: z.uuidv7('A category ID must be provided'),
+	idTechnique: z.uuidv7('A technique ID must be provided'),
+	idSubject: z.uuidv7('A subject ID must be provided'),
+	releaseDate: z
+		.string()
+		.regex(/^\d{4}-\d{2}-\d{2}$/, 'The date format must be YYYY-MM-DD')
+		.transform((dateString) => new Date(dateString))
+		.refine((date) => !isNaN(date.getTime()), {
+			message: 'Invalid date',
+		}),
 	dimensions: DimensionsSchema,
 	title: CreateTitleSchema,
 	description: CreateDescriptionSchema,
@@ -20,7 +26,7 @@ const CreateArtworkSchema = z.object({
 			priceWithFrame: z.number(),
 		})
 		.optional(),
-	keywords: z.array(z.string()),
+	keywords: z.array(z.uuidv7('Keyword ID must be a string')),
 });
 
 type ICreateArtwork = z.infer<typeof CreateArtworkSchema>;
